3671dc10e782697166902a63907d92ce6e269969
Daniel Macharia <danielhuios@gmail.com>
2022-10-18 09:35:51 +0300

    all tests passed

--- control_flow.rb
+++ control_flow.rb
@@ -1,7 +1,7 @@
 
 def admin_login(username, password)
   # your code here
-  if username == "admin" || username == "ADMIN" && password == "12345"
+  if (username == "admin" || username == "ADMIN") && password == "12345"
     "Access granted"
   else
     "Access denied"
